dcb37e23df515b2079e86557b5d5ecdfef2b772c
klimatt <klimatt.gu@gmail.com>
2024-07-15 19:24:25 +0200

    First working small loop wihtout magic

--- link.x
+++ link.x
@@ -1,30 +1,41 @@
-/* Memory layout of the LM3S6965 microcontroller */
 /* 1K = 1 KiBi = 1024 bytes */
 MEMORY
 {
-  FLASH : ORIGIN = 0x20010000, LENGTH = 32M - 0x10000
+  FLASH : ORIGIN = 0x20000000, LENGTH = 32M
   RAM : ORIGIN = 0x80000000, LENGTH = 16K
 }
 
 /* The entry point is the reset handler */
-ENTRY(Reset);
+ENTRY(main);
 
 EXTERN(RESET_VECTOR);
 
 SECTIONS
 {
-  . = 0x20010000;
-  .vector_table ORIGIN(FLASH) :
-  {
-    /* First entry: initial Stack Pointer value */
-    LONG(ORIGIN(RAM) + LENGTH(RAM));
-
-    /* Second entry: reset vector */
-    KEEP(*(.vector_table.reset_vector));
-  } > FLASH
-
   .text :
   {
+
     *(.text .text.*);
   } > FLASH
+
+  .test_var :
+  {
+     LONG(0xDEADBEEF);
+  } > FLASH
+
+ /* .data :
+  {
+    *(.data .data.*);
+  } > RAM
+
+  .bss : { *(.bss .bss.*) } > RAM
+*/
+/*
+  RAM_END = ORIGIN(RAM) + LENGTH(RAM) - 1;
+*/
+  /*.stack : AT(ORIGIN(RAM))
+  {
+    . += 0x1000;
+  } > RAM*/
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
 #![no_std]
 #![no_main]
 
+use core::arch::global_asm;
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub unsafe extern "C" fn Reset() -> ! {
-    let _x = 42;
+/*global_asm!(
+    ".section .init
+    .global _start
 
-    // can't return so we go into an infinite loop here
-    loop {}
+    _start:
+        la sp, _stack_top
+        call main
+        j .
+
+    .section .stack
+    _stack_top:
+        .space 0x1000
+"
+);*/
+
+#[no_mangle]
+pub unsafe extern "C" fn main() -> ! {
+
+
+    let _x = 42u32 ;
+
+    //panic!();
+
+        // can't return so we go into an infinite loop here
+    loop {
+        core::arch::asm!("nop");
+    }
 }
 
 // The reset vector, a pointer into the reset handler
-#[link_section = ".vector_table.reset_vector"]
+/*#[link_section = ".vector_table.reset_vector"]
 #[no_mangle]
 pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+*/
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
